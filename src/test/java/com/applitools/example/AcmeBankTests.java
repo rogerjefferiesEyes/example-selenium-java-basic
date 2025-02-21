97820ce092a8f45786a9fc1caa9bd5a86d212f03
Roger Jefferies <roger.jefferies@applitools.com>
2025-02-21 11:56:44 -0500

    PR Demo

--- src/test/java/com/applitools/example/AcmeBankTests.java
+++ src/test/java/com/applitools/example/AcmeBankTests.java
@@ -70,6 +70,7 @@ public class AcmeBankTests {
             );
             config.setLayoutBreakpoints(true);
         }
+
 //        config.setBranchName("rogerjefferiesEyes/example-selenium-java-basic/main");
 //        config.setBaselineBranchName("rogerjefferiesEyes/example-selenium-java-basic/main");
         eyes.setConfiguration(config);
